89e6cb6f72b60bbbe88639e9d56cb557155e8dc9
Rohan Singh <rohan.singh.en@gmail.com>
2025-07-04 17:01:03 +0530

    update button logic

--- client/src/components/ui/Button/Button.jsx
+++ client/src/components/ui/Button/Button.jsx
@@ -1,25 +1,36 @@
-import { twMerge } from 'tailwind-merge';
+import { cva } from "class-variance-authority"
+import { cn } from "../../../utils/cn"
+import PropTypes from "prop-types"
 
-export default function Button({
-  children,
-  variant = 'primary',
-  onClick,
-  className
-}) {
-  const base =
-    'px-4 py-0.5 rounded-[10px] border transition-colors duration-200 hover:cursor-pointer';
-  const variants = {
-    primary: 'bg-lime-400 text-black border-lime-400 hover:bg-lime-500',
-    outline:
-      'bg-transparent text-white border-white hover:bg-white hover:text-black'
-  };
+const buttonVariants = cva(
+  "px-4 py-0.5 rounded-[10px] border transition-colors duration-200 hover:cursor-pointer",
+  {
+    variants: {
+      variant: {
+        primary: "bg-lime-400 text-black border-lime-400 hover:bg-lime-500",
+        outline: "bg-transparent text-white border-white hover:bg-white hover:text-black",
+      },
+    },
+    defaultVariants: {
+      variant: "primary",
+    },
+  }
+)
 
+function Button({ className, variant, onClick, ...props }) {
   return (
     <button
       onClick={onClick}
-      className={twMerge(`${base} ${variants[variant]} ${className || ''}`)}
-    >
-      {children}
-    </button>
-  );
+      className={cn(buttonVariants({ variant, className }))}
+      {...props}
+    />
+  )
 }
+
+Button.propTypes = {
+  className: PropTypes.string,
+  variant: PropTypes.oneOf(["primary", "outline"]),
+  onClick: PropTypes.func,
+}
+
+export default Button
