a3bc1dedf3a03eb67434be94fdf482f90485c21f
Kevish Sewliya <129868668+Kevish07@users.noreply.github.com>
2025-08-08 22:52:22 +0530

    data models updated

--- Server/src/models/project.model.js
+++ Server/src/models/project.model.js
@@ -6,6 +6,12 @@ const projectSchema = new mongoose.Schema({
   image_url: String,
   repository_url: String,
   organization: { type: mongoose.Schema.Types.ObjectId, ref: 'Organization' },
+  readme: String,
+  stars: { type: Number, default: 0 },
+  tech_stack: [String],
+  tags: { type: [String], index: true },
+  live_demo_url: String,
+  assigned_mentor: { type: mongoose.Schema.Types.ObjectId, ref: 'Mentor' }
 }, { timestamps: true });
 
 export default mongoose.model('Project', projectSchema);
