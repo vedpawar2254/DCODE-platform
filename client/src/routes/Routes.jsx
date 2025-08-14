6bdfe40b754f4cbb657008adaa107eda74daa2e7
thegamingclan405@gmail.com <nimitsharma@Nimits-MacBook-Pro.local>
2025-08-15 01:07:45 +0530

    init

--- client/src/routes/Routes.jsx
+++ client/src/routes/Routes.jsx
@@ -4,7 +4,9 @@ import WaitList from '../pages/Waitlist/WaitList';
 import Home from '../pages/Home/Home';
 import Dashboard from "../pages/Dashboard/Dashboard"
 import ContactPage from '../pages/Contact/ContactPage';
-
+import { OnboardingPage } from '../pages/Onboarding/OnboardingPage';
+import { element } from 'prop-types';
+import { AskExperience } from '../components/Onboarding/AskExperience';
 
 
 export const routes = createBrowserRouter([
@@ -31,5 +33,23 @@ export const routes = createBrowserRouter([
   {
     path:"/contact",
     element:<ContactPage/>
+  },
+  {
+    path: '/onboarding',
+    element: <OnboardingPage />,
+    children: [
+      {path: '/exp',
+      element: <AskExperience />},
+      {path: '/auth',
+        element: <Auth />
+      },
+      {path: '/createFork', element: <CreateFork />},
+      {path: '/createBranch', element: <CreateBranch />},
+      {path: '/createCommit', element: <CreateCommit />},
+      {path: '/createPullRunChecks', element: <createPullRunChecks />},
+      {path: '/end', element: <EndFlow />},
+      {path: '/checkStatus', element: <CheckStatus />},
+
+    ]
   }
 ]);
