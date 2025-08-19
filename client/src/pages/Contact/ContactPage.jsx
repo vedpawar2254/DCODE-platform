0e3ccb7701866f1aefff695eca42d7d626c10293
Ved Pawar <ved.pawar2024@nst.rishihood.edu.in>
2025-08-19 23:26:46 +0530

    .

--- client/src/pages/Contact/ContactPage.jsx
+++ client/src/pages/Contact/ContactPage.jsx
@@ -1,12 +1,17 @@
 import ContactFormPanel from './ContactFormPanel';
 import ContactInfoPanel from './ContactInfoPanel';
+import NavBar from '../../components/layout/NavBar/NavBar';
 
 const ContactPage = () => {
-  return (
+  return (<>
+     <NavBar/>
     <div className="flex flex-col items-center justify-center md:flex-row min-h-screen px-4 sm:px-6 md:px-8">
+     
       <ContactInfoPanel />
       <ContactFormPanel />
     </div>
+
+    </>
   );
 };
 
